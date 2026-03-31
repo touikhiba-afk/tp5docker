55e1068617f98dddbf9d29c923ff28a7f9db736d
hiba <touikhiba@gmail.com>
2026-03-31 10:09:11 +0100

    deploy

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@ try { docker stop tp5docker } catch {}
 try { docker rm tp5docker } catch {}
 
 # Tirer la dernière image
-docker pull votre_UsernameDocker/flask_devops_cd:latest
+docker pull hibatouik/flask_devops_cd:latest
 
 # Lancer le conteneur
-docker run -d --name tp5docker -p 5000:5000 votre_UsernameDocker/tp5docker:latest
+docker run -d --name tp5docker -p 5000:5000 hibatouik/tp5docker:latest
